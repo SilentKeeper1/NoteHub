380e13a6ee56e13c0f5d818542894418033000ee
SilentKeeper1 <egor.lloyd@gmail.com>
2025-02-21 19:48:37 +0200

    project websites

--- NoteHub/main.py
+++ NoteHub/main.py
@@ -24,26 +24,26 @@ def html_page():
 def javascript_page():
     return render_template('javascript.html')
 
-# @app.route('/check/')
-# def check_server():
-#     try:
-#         flask_installed = subprocess.run(["python", "-c", "import flask"], capture_output=True, text=True)
-#         flask_version = subprocess.run(["python", "-m", "flask", "--version"], capture_output=True, text=True)
-#         templates_exist = os.path.exists("templates")
-#         index_exist = os.path.exists("templates/check.html")
-#
-#         result = "=== Перевірка сервера ===\n"
-#         result += "Flask встановлено ✅\n" if flask_installed.returncode == 0 else "Flask НЕ встановлено ❌\n"
-#         result += f"Версія Flask: {flask_version.stdout.strip()}\n" if flask_version.returncode == 0 else "Не вдалося отримати версію Flask ❌\n"
-#         result += "Папка templates існує ✅\n" if templates_exist else "Папки templates немає ❌\n"
-#         result += "index.html існує ✅\n" if index_exist else "Файл index.html відсутній ❌\n"
-#
-#
-#         print(result)
-#
-#         return jsonify({"status": "ok", "message": "Перевірка виконана! Дивись консоль PyCharm."})
-#     except Exception as e:
-#         return jsonify({"status": "error", "message": str(e)})
+@app.route('/check/')
+def check_server():
+    try:
+        flask_installed = subprocess.run(["python", "-c", "import flask"], capture_output=True, text=True)
+        flask_version = subprocess.run(["python", "-m", "flask", "--version"], capture_output=True, text=True)
+        templates_exist = os.path.exists("templates")
+        index_exist = os.path.exists("templates/check.html")
+
+        result = "=== Перевірка сервера ===\n"
+        result += "Flask встановлено ✅\n" if flask_installed.returncode == 0 else "Flask НЕ встановлено ❌\n"
+        result += f"Версія Flask: {flask_version.stdout.strip()}\n" if flask_version.returncode == 0 else "Не вдалося отримати версію Flask ❌\n"
+        result += "Папка templates існує ✅\n" if templates_exist else "Папки templates немає ❌\n"
+        result += "index.html існує ✅\n" if index_exist else "Файл index.html відсутній ❌\n"
+
+
+        print(result)
+
+        return jsonify({"status": "ok", "message": "Перевірка виконана! Дивись консоль PyCharm."})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
